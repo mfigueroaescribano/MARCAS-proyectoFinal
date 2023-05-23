c23dbc086bf57da4e34302fc2cf29d79843759a0
Miguel Figueroa Escribano <miguelfigueroaescribano@gmail.com>
2023-05-23 23:40:49 +0200

    Update app.py

--- .venv/app.py
+++ .venv/app.py
@@ -4,4 +4,10 @@ from flask import Flask, render_template, abort, redirect, request
 
 app = Flask(__name__)
 
-app.run("0.0.0.0", 5000, debug=True)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello, world!</h1>'
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000, debug=True)
