48c26878a57efd9aa253fc724b7fe8e894497f09
Miguel Figueroa Escribano <miguelfigueroaescribano@gmail.com>
2023-05-25 18:24:51 +0200

    Estructura con env y app.py funcionando

--- .venv/app.py
+++ .venv/app.py
@@ -4,10 +4,13 @@ from flask import Flask, render_template, abort, redirect, request
 
 app = Flask(__name__)
 
+@app.route('/',methods=["GET","post"])
+def inicio():
+    return render_template("base.html")
 
-@app.route('/')
-def hello_world():
-    return '<h1>Hello, world!</h1>'
+@app.route('/error')
+def error():
+    return abort(404)
 
 if __name__ == '__main__':
     app.run("0.0.0.0", 5000, debug=True)
