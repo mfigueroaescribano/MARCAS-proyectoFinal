d821c6cc35b03bacbe6f351d152cca6d50cbc544
Miguel Figueroa Escribano <miguelfigueroaescribano@gmail.com>
2023-05-25 23:21:49 +0200

    Create templates

--- .venv/app.py
+++ .venv/app.py
@@ -6,7 +6,19 @@ app = Flask(__name__)
 
 @app.route('/',methods=["GET","post"])
 def inicio():
-    return render_template("base.html")
+    return render_template("index.html")
+
+@app.route('/list',methods=["GET","post"])
+def list():
+    return render_template("list.html")
+
+@app.route('/detalles',methods=["GET","post"])
+def detalles():
+    return render_template("detalles.html")
+
+@app.route('/filtrar',methods=["GET","post"])
+def filtrar():
+    return render_template("filtrar.html")
 
 @app.route('/error')
 def error():
