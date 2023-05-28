fe9847f8d055829cb8ae4d48e42b9d6a597d8fe3
Miguel Figueroa Escribano <miguelfigueroaescribano@gmail.com>
2023-05-28 19:52:58 +0200

    detalles preview

--- .venv/app.py
+++ .venv/app.py
@@ -5,6 +5,7 @@ from flask import *
 app = Flask(__name__)
 url = "https://webfonts.googleapis.com/v1/webfonts"
 key = {"key": "AIzaSyDfz0lHOk42n4-7hPBV_j7xj3SrBu9b5A8"}
+key2 = "AIzaSyDfz0lHOk42n4-7hPBV_j7xj3SrBu9b5A8"
 
 
 @app.route('/',methods=["GET","post"])
@@ -23,9 +24,20 @@ def list():
     else:
         return "Error al hacer la solicitud: {}".format(response.status_code)
 
-@app.route('/detalles',methods=["GET","post"])
-def detalles():
-    return render_template("detalles.html")
+@app.route('/detalles',methods=["GET","POST"])
+def search_fonts():
+    if request.method == 'POST':
+        search_query = request.form['font_name']
+        font_details = get_font_details(search_query)
+        return render_template('detalles.html', font_name=search_query, font_details=font_details)
+    return render_template('detalles.html')
+
+def get_font_details(query):
+    response = requests.get(f'{url}?family={query}&key={key2}')
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    return None
 
 @app.route('/filtrar',methods=["GET","post"])
 def filtrar():
